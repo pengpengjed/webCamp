20b818be9aaa9f6d48d9423f95b0753c62e45959
zhangyupeng <zhangyupeng@haotaisoft.com>
2024-02-28 08:29:54 +0800

    20240228

--- code/5. JavaScript 模块化详解/前端模块化/3. commonjs/1. demo1/lib.js
+++ code/5. JavaScript 模块化详解/前端模块化/3. commonjs/1. demo1/lib.js
@@ -2,5 +2,12 @@ var x = 5;
 var addX = function (value) {
   return value + x;
 };
-module.exports.x = x;
-module.exports.addX = addX;
+
+// module当前模块 
+// module.exports 模块的exports属性, 对外接口
+// 加载某个模块就是加载该模块的 module.exports
+
+// module.exports.x = x;
+// module.exports.addX = addX;
+module.exports.x = x
+module.exports.addX = addX
